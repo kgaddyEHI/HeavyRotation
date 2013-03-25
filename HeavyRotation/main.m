24788a77a52f7a501349261203a4e4657cc38092
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-03-25 16:03:46 -0500

    init
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by kgaddy on 3/25/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
